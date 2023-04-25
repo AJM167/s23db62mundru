04f0d0c9e73f336abbf34578fe3f5c36779e2f3c
AJM167 <123146230+AJM167@users.noreply.github.com>
2023-04-24 22:55:00 -0500

    use a good message

--- models/books.js
+++ models/books.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose")
+
 const booksSchema = mongoose.Schema({
-    books_type: {type:String},     
-    books_year: {type:String},
-    books_price: { type: Number }
+    books_type: { type: String },     
+    books_year: { type: String },
+    books_price: {
+        type: Number,
+        min: [0, 'Price cannot be negative'],
+        max: [10000, 'Price cannot exceed 10000']
+    }
 }) 
 
 module.exports = mongoose.model("books", booksSchema)
--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var router = express.Router();
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
+/* GET update books page */
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('books', { title: 'Search Results books' });
@@ -12,10 +23,12 @@ router.get('/', books_controlers.books_view_all_Page );
 router.get('/detail', books_controlers.books_view_one_Page);
 module.exports = router;
 /* GET create books page */
-router.get('/create', books_controlers.books_create_Page);
+router.get('/create',secured, books_controlers.books_create_Page);
 
-router.get('/update', books_controlers.books_update_Page);
+router.get('/update',secured, books_controlers.books_update_Page);
 
 /* GET delete books page */
-router.get('/delete', books_controlers.books_delete_Page);
+router.get('/delete',secured, books_controlers.books_delete_Page);
+router.get('/update', secured,
+books_controlers.books_update_Page);
 
